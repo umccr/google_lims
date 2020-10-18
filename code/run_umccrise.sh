11e26ff5795424f6051af2a9e2c8497be98b4669
ohofmann <ohofmann72@gmail.com>
2020-10-19 10:47:38 +1100

    Updated resource configs

--- code/run_umccrise.sh
+++ code/run_umccrise.sh
@@ -6,7 +6,15 @@
 #PBS -l ncpus=1
 #PBS -l software=umccrise
 #PBS -l wd
+#PBS -l storage=gdata/gx8
+export PATH=/opt/bin:/bin:/usr/bin:/opt/pbs/default/bin
 
-#source /g/data3/gx8/extras/umccrise_2019_Aug/load_umccrise.sh
-source /g/data/gx8/extras/umccrise/load_umccrise.sh
-umccrise . --no-igv -j 28 --cluster-auto
+# Dev version
+source /g/data3/gx8/extras/umccrise_2019_Nov/load_umccrise.sh
+
+# Alpha version
+#source /g/data/gx8/extras/umccrise_017_2020_Jan/load_umccrise.sh
+
+# Production version
+#source /g/data/gx8/extras/umccrise/load_umccrise.sh
+umccrise . -j 16 --cluster-auto --unlock
